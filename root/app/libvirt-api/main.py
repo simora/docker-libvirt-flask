937f8da168e9d00b6b717ceb7f027d5b88c960b7
Simora Altreus <simora@simora.haus.marklehaus.com>
2020-08-09 15:28:08 -0400

    /info route

--- root/app/libvirt-api/main.py
+++ root/app/libvirt-api/main.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:blue'>Welcome to the Libvirt Flask API</h1>"
+
+@app.route("/info")
+def info():
+    return "<h1 style='color:blue'>Welcome to the Libvirt Flask API</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
