672c6a3515b167df8525237205a9e6be5baa24b9
Simora Altreus <simora@simora.haus.marklehaus.com>
2020-08-09 19:38:20 -0400

    use g

--- root/app/libvirt-api/main.py
+++ root/app/libvirt-api/main.py
@@ -1,7 +1,7 @@
 import yaml, traceback, sys, json, inspect
 
 from typing import Dict
-from flask import Flask, jsonify
+from flask import Flask, jsonify, g
 app = Flask(__name__)
 
 class LibvirtHost(dict):
@@ -28,16 +28,12 @@ class Config(object):
         retVal['hosts'] = self.hosts
         return retVal
 
-@app.on_event('startup')
-def startup():
-    AppConfig = Config(configFile='/config/config.yaml')
-
 @app.errorhandler(Exception)
 def handle_exception(e):
     etype, value, tb = sys.exc_info()
     print(traceback.print_exception(etype, value, tb))
     resp = {}
-    resp['config'] = Config(configFile='/config/config.yaml').to_dict()
+    resp['config'] = g.config
     resp['error'] = str(traceback.format_exc())
     return jsonify(resp), 500
 
@@ -48,7 +44,8 @@ def hello():
 @app.route("/info")
 def info():
     if len(AppConfig.hosts) > 0:
-        return f"<h1 style='color:blue'>Name: {AppConfig.hosts[0]['name']}</h1>"
+        return f"<h1 style='color:blue'>Name: {g.config.hosts[0]['name']}</h1>"
 
 if __name__ == "__main__":
+    g.config = Config(configFile='/config/config.yaml')
     app.run(host='0.0.0.0')
