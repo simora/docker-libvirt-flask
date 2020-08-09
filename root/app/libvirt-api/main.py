e516f2f897fe15fc9bb14b42c08790cd7346b8a1
Simora Altreus <simora@simora.haus.marklehaus.com>
2020-08-09 17:07:44 -0400

    .

--- root/app/libvirt-api/main.py
+++ root/app/libvirt-api/main.py
@@ -36,7 +36,7 @@ class Config(object):
 def handle_exception(e):
     etype, value, tb = sys.exc_info()
     print(traceback.print_exception(etype, value, tb))
-    return jsonify(error=str(traceback.format_exc(), config=Config(configFile='/config/config.yaml').to_dict())), 500
+    return jsonify(error=str(traceback.format_exc()), config=Config(configFile='/config/config.yaml').to_dict()), 500
 
 @app.route("/")
 def hello():
